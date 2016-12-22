e2f97a28c40ce5c8acdd3db8032d0da08b3e3446
Kristi Murphy <kristilmurphy@gmail.com>
2016-12-22 09:36:49 -0500

    improve seeds, fix email styling, fix levels styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,19 +18,19 @@ Challenge.destroy_all
 Level.destroy_all
 
 
-  Level.create(title: "Basic B",
-                       description: "Introduction to PHP 1",
+  Level.create(title: "Level 1: PHP Basics",
+                       description: "Getting started! Every hero has to start somewhere. Let's learn how to say 'Hello World' and declare variables.",
                        level_num:1)
-  Challenge.create(title: "Challenge 1",
-                              description: "A PHP script starts with <?php and ends with ?>:PHP script that uses a built-in PHP function 'echo' to output the text 'Hello World!'  ' on a web page:",
+  Challenge.create(title: "Challenge 1: Hello World!",
+                              description: "A PHP script starts with <?php and ends with ?> To print words, PHP uses a built-in function 'echo' to output the text. For example, <?php echo 'Hello World';?> Would print 'Hello World!'  ' on a web page",
                               level_id: Level.last.id,
-                              question: "use echo to print 'HELLO WORLD'",
-                               answer: "echo 'HELLO WORLD';")
-  Challenge.create(title: "Challege 2",
-                               description:"a variable starts with the $ sign, followed by the name of the variable:",
+                              question: "Use echo to print 'HELLO WORLD'",
+                               answer: "<?php echo 'Hello World';?>")
+  Challenge.create(title: "Challege 2: Declare a variable",
+                               description:"With your new super powers you can create new words! To declare a variable start with the $ sign, followed by the name of the variable. For example, if you wanted to declare variable 'friendly neighborhood' to value 'spiderman', you would write "$friendlyneighborhood='spiderman';" ",
                                level_id: Level.last.id,
-                               question:"Declare variable x and assign it a value of porky",
-                               answer: "$x='porky';")
+                               question:"Some superheroes keep their identity a secret! Declare variable 'identity' and assign it a value of 'secret'",
+                               answer: "$identity='secret';")
 
   Level.create(title: "Intermedio B",
                         description: "Intro to php 2",
